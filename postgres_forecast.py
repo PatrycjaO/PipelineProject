35483614fe179336b257042307ebedb1d86473c0
Pati O <ottawa.pa@gmail.com>
2021-10-05 20:04:59 +0200

    forecast postgres

---
+++ postgres_forecast.py
@@ -0,0 +1,53 @@
+import csv
+import os
+import sys
+import psycopg2
+import psycopg2.extras
+
+def write2database(inputFile):
+
+    DB_HOST = '****'
+    DB_NAME = '****'
+    DB_USER = '****'
+    DB_PASS = '****'
+    DB_PORT = '****'
+
+    try:
+        conn = psycopg2.connect(dbname=DB_NAME, user=DB_USER, password=DB_PASS, host=DB_HOST, port=DB_PORT)
+        print('Connection succesful')
+    except:
+        print('Connection failed')
+
+    cursor = conn.cursor()
+
+    try:
+        cursor.execute("""
+        CREATE TABLE if not exists energydata (
+            "timestamp" integer,
+            "regionCode" integer NOT NULL,
+            "region" text,
+            "wind_speed_m/s" real,
+            "wind_direction" real,
+            "wind_gust" real,
+            "cloudCover_pct" real,
+            "weathercode" integer
+        );
+        """)
+        print('Table created')
+    except:
+        print('Table not created')
+
+    with open(inputFile, 'r') as f:
+        next(f)
+        cursor.copy_from(f, 'energydata', sep=',')
+
+    conn.commit()
+    cursor.close()
+    conn.close()
+
+if __name__ == "__main__":
+    INPUT_FILE = sys.argv[1] # this is the csv file to move to PostgreSQL Database
+    try:
+        write2database(INPUT_FILE)
+    except:
+        print("Error writing csv to database")
