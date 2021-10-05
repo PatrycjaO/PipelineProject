2b7845df00c1de2c5da051b36400bf0c10907444
Pati O <ottawa.pa@gmail.com>
2021-10-05 11:09:33 +0200

    streamlit corrected

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import psycopg2
 import sshtunnel
-import streamlit
+import streamlit as st
 
 sshtunnel.SSH_TIMEOUT = 5.0
 sshtunnel.TUNNEL_TIMEOUT = 5.0
@@ -8,14 +8,14 @@ sshtunnel.TUNNEL_TIMEOUT = 5.0
 try:
     with sshtunnel.SSHTunnelForwarder(
         ('ssh.pythonanywhere.com'),
-        ssh_username=PA_USER, ssh_password=PA_PASS,
-        remote_bind_address=(DB_HOST, DB_PORT)) as tunnel:
+        ssh_username=st.secrets["PA_USER"], ssh_password=st.secrets["PA_PASS"],
+        remote_bind_address=(st.secrets["DB_HOST"], st.secrets["DB_PORT"])) as tunnel:
         connection = psycopg2.connect(
-            user=DB_USER, password=DB_PASS,
+            user=st.secrets["DB_USER"], password=st.secrets["DB_PASS"],
             host='127.0.0.1', port=tunnel.local_bind_port,
-            database=DB_NAME,
+            database=st.secrets["DB_NAME"],
         )
-        print('ssh connection established')
+        st.write('ssh connection established')
         connection.close()
 except:
-    print('ssh connection failed')
+    st.write('ssh connection failed')
