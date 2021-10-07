9bc3fca61224b7a5f4d438644c7534b37704726f
Pati O <ottawa.pa@gmail.com>
2021-10-07 13:25:44 +0200

    test streamlit

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,13 +10,14 @@ try:
         ('ssh.pythonanywhere.com'),
         ssh_username=st.secrets["PA_USER"], ssh_password=st.secrets["PA_PASS"],
         remote_bind_address=(st.secrets["DB_HOST"], st.secrets["DB_PORT"])) as tunnel:
+        st.write('ssh connected')
         connection = psycopg2.connect(
             user=st.secrets["DB_USER"], password=st.secrets["DB_PASS"],
             host='127.0.0.1', port=tunnel.local_bind_port,
             database=st.secrets["DB_NAME"],
         )
         #@st.cache(allow_output_mutation=True, hash_funcs={"_thread.RLock": lambda _: None})
-        st.print('connection initiated')
+        st.write('connection initiated')
 
         #@st.cache(ttl=600)
         def run_query(query):
@@ -24,11 +25,11 @@ try:
                 cur.execute(query)
                 return cur.fetchall()
 
-        rows = run_query("SELECT * FROM energytable;")
+        rows = run_query("SELECT * FROM energydata;")
 
         # Print results.
         for row in rows:
-            st.write(f"{row[0]} has a :{row[1]}:")
+            st.write(row)
             #st.write('ssh connection established')
         connection.close()
 except:
