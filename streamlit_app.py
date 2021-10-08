bce3ed9cfa50fe40f9184150289434464184112e
Pati O <ottawa.pa@gmail.com>
2021-10-08 09:16:57 +0200

    streamlit interface

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import psycopg2
 import sshtunnel
 import streamlit as st
+import pandas as pd
 
 sshtunnel.SSH_TIMEOUT = 5.0
 sshtunnel.TUNNEL_TIMEOUT = 5.0
@@ -17,20 +18,39 @@ try:
             database=st.secrets["DB_NAME"],
         )
         #@st.cache(allow_output_mutation=True, hash_funcs={"_thread.RLock": lambda _: None})
-        st.write('connection initiated')
+        st.title('Wind & Wind Power Generation Monitor')
 
-        #@st.cache(ttl=600)
-        def run_query(query):
-            with connection.cursor() as cur:
-                cur.execute(query)
-                return cur.fetchall()
+        facttable = pd.read_sql("select * from  \"facttable\"", connection)
+        energy = pd.read_sql("select * from \"energydata\"", connection)
+        forecast = pd.read_sql("select * from \"forecastdata\"", connection)
 
-        rows = run_query("SELECT * FROM energydata;")
+        def to_datetime(df):
 
-        # Print results.
-        for row in rows:
-            st.write(row)
-            #st.write('ssh connection established')
+            df['timestamp'] = pd.to_datetime(df['timestamp'], unit='s')
+            return df
+    
+        energy = to_datetime(energy)
+        forecast = to_datetime(forecast)
+
+        st.subheader('Locations Map')
+        st.map(facttable)
+
+
+        if st.checkbox('Show raw data'):
+            st.write('Lookup Table')
+            st.dataframe(facttable)
+            st.write('Forecast')
+            st.dataframe(forecast)
+            st.write('Energydata')
+            st.dataframe(energy)
+
+        st.subheader('Filter Forecast Time')
+
+        hour_to_filter = st.slider('hour', 0, 23, 17)
+        filtered_hour = forecast[(forecast['timestamp'].dt.hour == hour_to_filter)]
+        st.subheader('Wind forecast at %s:00' % hour_to_filter)
+        st.dataframe(filtered_hour)    
+
         connection.close()
 except:
     st.write('ssh connection failed')
